56d59b0fd189c63b6e5078e114a77d7bda333ce3
arnold-aswan <fredyaswan@gmail.com>
2023-11-20 16:37:45 +0300

    added sidebar functionality to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,19 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { NAV_LINKS } from "@/constants";
 import Button from "./Button";
 
 function Navbar() {
+  const [showSideBar, setShowSidebar] = useState(false);
+
+  const openSideBar = () => {
+    setShowSidebar(true);
+  };
+  const closeSideBar = () => {
+    setShowSidebar(false);
+  };
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -16,7 +25,24 @@ function Navbar() {
         />
       </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul
+        className={`hiddesn h-fulls gap-12 lg:flex ${
+          showSideBar
+            ? "flex flex-col gap-5 pt-6 fixed top-0 right-0 h-full z-20  bg-slate-200 w-[15rem] transition-all duration-200 ease-in-out"
+            : "hidden"
+        }`}
+      >
+        {showSideBar && (
+          <li>
+            <Image
+              src="close.svg"
+              alt="close"
+              width={34}
+              height={34}
+              onClick={closeSideBar}
+            />
+          </li>
+        )}
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
@@ -43,7 +69,14 @@ function Navbar() {
         width={34}
         height={34}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={openSideBar}
       />
+      {showSideBar && (
+        <div
+          className="fixed top-0 left-o right-0 bottom-0 w-full h-full z-10 bg-[rgba(211,211,211,0.3)]"
+          onClick={closeSideBar}
+        />
+      )}
     </nav>
   );
 }
