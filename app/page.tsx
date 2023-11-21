821dc85eb3b0d09372b719ca4c01a2d2ededb7f3
arnold-aswan <fredyaswan@gmail.com>
2023-11-21 13:17:56 +0300

    added camp component to pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Camp from "@/components/Camp";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <>
       <Hero />
-    </main>
+      <Camp />
+    </>
   );
 }
