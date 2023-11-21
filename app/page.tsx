ce0cb18ff0e81015e012dd677071b78b0be752bc
arnold-aswan <fredyaswan@gmail.com>
2023-11-21 16:06:12 +0300

    added get app section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Camp from "@/components/Camp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Camp />
       <Guide />
       <Features />
+      <GetApp />
     </>
   );
 }
